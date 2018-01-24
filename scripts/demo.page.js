f257eab1df05b472a30c968b69b7cf0a094c55f8
ecscstatsconsulting <33598754+ecscstatsconsulting@users.noreply.github.com>
2018-01-24 16:21:11 -0500

    Update demo.page.js

--- scripts/demo.page.js
+++ scripts/demo.page.js
@@ -1,9 +1,19 @@
-﻿$(document).ready(function () {
-    excel = new ExcelGen({
-        "src_id": "test_table",
-        "show_header": true
+$(document).ready(function () {
+    $("#generate-excel-basic").click(function () {
+		excel = new ExcelGen({
+			"src_id": "basic_table",
+			"show_header": true,
+			"type": "table"
+		});
+        excel.generate();
     });
-    $("#generate-excel").click(function () {
+    $("#generate-excel-formatted").click(function () {
+		excel = new ExcelGen({
+			"src_id": "formatted_table",
+			"show_header": true,
+			"type": "table",
+			"column_formats": ["1", "1", "4", "6", "18"] 
+		});
         excel.generate();
     });
 });
